ed4b7951c9dc50ba1f08cb7dce480e916b4c0b54
Usuario <Usuario@COMPU-ULI>
2024-10-05 16:34:55 -0300

    Agrego Enums TipoDeCarta (ahora si)

--- TP-Pseudoequipo/src/tp2prueba/Prueba.java
+++ TP-Pseudoequipo/src/tp2prueba/Prueba.java
@@ -3,8 +3,7 @@ package tp2prueba;
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- TP-Pseudoequipo/src/tp2prueba/TipoDeCarta.java
+++ TP-Pseudoequipo/src/tp2prueba/TipoDeCarta.java
@@ -1,17 +1,11 @@
 package tp2prueba;
-//public enum Tipo{A,B,C,D,E,F,G};
-/**
-/**
- * Atributos:
-*tipo: Tipo de carta(bloquear ficha, anular casillero, etc.).
-descripcion: Explicación del efecto de la carta.
-***********************
-Métodos:
-aplicar(juego, jugador, objetivo): Aplica el efecto de la carta, afectando el juego o a otro jugador.
-*/
-//public class Carta {
-////    private Tipo Nombre;
-//
-//
-//
-//}
+
+public enum TipoDeCarta {
+	PERDER_TURNO,
+	BLOQUEAR_FICHA,
+	ANULAR_CASILLERO,
+	VOLVER_JUGADA_ANTERIOR,
+	CAMBIAR_COLOR_FICHA,
+	// TODO: agregar 2 tipos de carta mas
+}
+
