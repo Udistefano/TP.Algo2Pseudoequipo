16d2030d6403432223b21c347056ccc57f93957c
Udistefano <udistefano@fi.uba.ar>
2024-11-07 17:54:31 -0300

    Actualizo carta abstracta con toString, hash, equals y su id

--- TP-Pseudoequipo/src/Cartas/Carta.java
+++ TP-Pseudoequipo/src/Cartas/Carta.java
@@ -1,31 +1,74 @@
 package Cartas;
 
+import java.util.Objects;
+
 import Main.TipoDeCarta;
 
 public abstract class  Carta {
     //ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
-    //ATRIBUTOS -----------------------------------------------------------------------------------------------
+    
+	private static long idActual = 1L;
+	
+	//ATRIBUTOS -----------------------------------------------------------------------------------------------
     private TipoDeCarta tipo = null;
+    private Long id = null;
 
     //CONSTRUCTORES -------------------------------------------------------------------------------------------
-    public Carta(TipoDeCarta tipo) throws Exception {
+    protected Carta(TipoDeCarta tipo) throws Exception {
         if (tipo == null) {
             throw new Exception("El tipo de carta no puede ser nulo");
         }
 
         this.tipo = tipo;
+        this.id = idActual++;
     }
 
     //METODOS DE CLASE ----------------------------------------------------------------------------------------
-    //METODOS GENERALES ---------------------------------------------------------------------------------------
-    //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
-    public void realizarHabilidadEspecial() {
-
+    
+    private static Long getIdActual() {
+    	return Carta.idActual++;
     }
+    
+    //METODOS GENERALES ---------------------------------------------------------------------------------------
+    
+    @Override
+    public String toString() {
+    	// TODO Auto-generated method stub
+    	return super.toString();
+    }
+    
+    @Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Carta other = (Carta) obj;
+		return Objects.equals(id, other.id);
+	}
+    
+    //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
+	public void realizarEfecto(){
+    	
+    }
+	
 
     //GETTERS SIMPLES -----------------------------------------------------------------------------------------
     public TipoDeCarta getTipo() {
         return this.tipo;
     }
+    
+    public Long getId() {
+    	return this.id;
+    }
+    
+    
     //SETTERS SIMPLES -----------------------------------------------------------------------------------------
 }
