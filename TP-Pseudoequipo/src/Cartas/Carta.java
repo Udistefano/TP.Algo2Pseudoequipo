c42586c338b807a8588efb95b81eca855df11699
aclopez <aclopez@fi.uba.ar>
2024-11-08 15:34:45 -0300

    Documentar metodos de clase Carta

--- TP-Pseudoequipo/src/Cartas/Carta.java
+++ TP-Pseudoequipo/src/Cartas/Carta.java
@@ -3,12 +3,9 @@ package Cartas;
 import java.util.Objects;
 
 import Jugadas.Jugada;
-import Main.TipoDeCarta;
-
 
 public abstract class Carta {
 //ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
-	
 	private static Long IdActual = 1L;
 	
 //ATRIBUTOS -----------------------------------------------------------------------------------------------
@@ -17,35 +14,63 @@ public abstract class Carta {
 	protected String titulo = null;
 	
 //CONSTRUCTORES -------------------------------------------------------------------------------------------
-	
+
+    /**
+     * pre: --
+     * post: inicializa una carta con el titulo por defecto y el id actual
+     */
 	protected Carta() {
-		this.titulo = getTituloPorDefecto();
+		this.titulo = this.getTituloPorDefecto();
 		this.id = Carta.getIdActual();
 	}
-	
-	protected Carta(String titulo) {
+
+    /**
+     * pre:
+     * @param titulo no puede ser nulo, ni vacio
+     * @throws Exception si titulo es nulo o vacio
+     * post: inicializa una carta con el titulo pasado por parametro y el id actual
+     */
+	protected Carta(String titulo) throws Exception {
+
 		this.titulo = titulo;
 		this.id = Carta.getIdActual();
 	}
 		
 //METODOS DE CLASE ----------------------------------------------------------------------------------------
-	
+
+    /**
+     * pre: --
+     * @return el id actual de la carta
+     */
 	private static Long getIdActual() {
 		return Carta.IdActual++;
 	}
 	
 //METODOS GENERALES ---------------------------------------------------------------------------------------
-	
+
+    /**
+     * pre: --
+     * @return una cadena que muestra el titulo de la carta y su id
+     */
 	@Override
 	public String toString() {
 		return this.getTitulo() + " (" + this.id + ")";
 	}
 
+    /**
+     * pre: --
+     * @return el hash code de la carta
+     */
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
+    /**
+     * pre:
+     * @param obj el cual a comparar con la carta actual
+     * @return verdadero si obj equivale a la carta actual, falso si no
+     */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,17 +84,33 @@ public abstract class Carta {
 	}
 	
 //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
-	
+
+    /**
+     * pre: --
+     * @return el titulo por defecto de la carta
+     */
 	protected abstract String getTituloPorDefecto();
-	
+
+    /**
+     * pre: --
+     * @return la jugada de la carta
+     */
 	public abstract Jugada getJugada();
 	
 //GETTERS SIMPLES -----------------------------------------------------------------------------------------
 
+    /**
+     * pre: --
+     * @return el titulo de la carta
+     */
 	public String getTitulo() {
 		return this.titulo;
 	}
-	
+
+    /**
+     * pre: --
+     * @return el id de la carta
+     */
 	public Long getId() {
 		return this.id;
 	}
