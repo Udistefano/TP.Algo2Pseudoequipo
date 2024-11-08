5521f386934ad2173caab5aa5c8232787c86d8d9
aclopez <aclopez@fi.uba.ar>
2024-11-08 16:36:48 -0300

    Agregar mas metodos documentados a TDA Turno

--- TP-Pseudoequipo/src/Main/Turno.java
+++ TP-Pseudoequipo/src/Main/Turno.java
@@ -1,45 +1,112 @@
 package Main;
 
 public class Turno {
-//ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
-//ATRIBUTOS -----------------------------------------------------------------------------------------------
-	
-	private Jugador jugador = null;
-	private int cantidadDeSubTurnos = 1;
+	//ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
+	//ATRIBUTOS -----------------------------------------------------------------------------------------------
+	private int cantidadDeSubturnos = 1;
 	private int bloqueosRestantes = 0;
-	
-//CONSTRUCTORES -------------------------------------------------------------------------------------------
-//METODOS DE CLASE ----------------------------------------------------------------------------------------
-//METODOS GENERALES ---------------------------------------------------------------------------------------
-//METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
-	
-	public void incrementarBloqueosRestantes(int cantidadDeBloqueos) {
+	private Jugador jugador = null;
+
+	//CONSTRUCTORES -------------------------------------------------------------------------------------------
+	//METODOS DE CLASE ----------------------------------------------------------------------------------------
+	//METODOS GENERALES ---------------------------------------------------------------------------------------
+	//METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
+
+	/**
+	 * pre:
+	 * @param jugador no puede ser nulo
+	 * @throws Exception si jugador es nulo
+	 * post: inicializa el turno con el jugador pasado por parametro, con un subturno y ningun bloqueo
+	 *       restante
+	 */
+	public Turno(Jugador jugador) throws Exception {
+		if (jugador == null) {
+			throw new Exception("El jugador no puede ser nulo");
+		}
+		this.jugador = jugador;
+	}
+
+	/**
+	 * pre:
+	 * @param cantidadDeBloqueos no puede ser negativo
+	 * post: le suma a la cantidad de bloqueos restantes del turno, la cantidad pasada por parametro
+	 */
+	public void incrementarBloqueosRestantes(int cantidadDeBloqueos) throws Exception {
+		// FIXME: la cantidad de bloqueos debe ser positiva?
+//        if (cantidadDeBloqueos < 0) {
+//            throw new Exception("La cantidad de bloqueos no puede ser negativa");
+//        }
 		this.bloqueosRestantes += cantidadDeBloqueos;
 	}
-	
+
+	/**
+	 * pre: --
+	 * post: si al turno le quedan bloqueos restantes, le resta uno
+	 */
 	public void terminarTurno() {
 		if (this.bloqueosRestantes > 0) {
 			this.bloqueosRestantes--;
 		}
 	}
-	
+
+	/**
+	 * pre: --
+	 * post: incrementa en una la cantidad de subturnos del turno
+	 */
 	public void iniciarTurno() {
-		this.cantidadDeSubTurnos += 1;
+		this.agregarSubturno();
 	}
-//GETTERS SIMPLES -----------------------------------------------------------------------------------------
-	
+
+	/**
+	 * pre: --
+	 * post: incrementa en una la cantidad de subturnos del turno
+	 */
+	public void agregarSubturno() {
+		this.cantidadDeSubturnos++;
+	}
+
+	/**
+	 * pre: --
+	 * @return verdadero si al turno le quedan subturnos disponibles, falso si no le quedan
+	 */
+	public boolean haySubturnos() {
+		// FIXME: el profe cantidadDeSubturnos >= 0, no deberia ser > 0 ?????
+		return this.cantidadDeSubturnos >= 0;
+	}
+
+	//GETTERS SIMPLES -----------------------------------------------------------------------------------------
+
+	/**
+	 * pre: --
+	 * @return el jugador del turno
+	 */
 	public Jugador getJugador() {
-		return jugador;
+		return this.jugador;
 	}
-	
+
+	/**
+	 * pre: --
+	 * @return verdadero si al turno le quedan bloqueos restantes, falso si no
+	 */
 	public boolean estaBloqueado() {
 		return this.bloqueosRestantes <= 0;
 	}
-	
-	public int getCantidadDeSubTurnos() {
-		return this.cantidadDeSubTurnos;
+
+	/**
+	 * pre: --
+	 * @return la cantidad de subturnos
+	 */
+	public int getCantidadDeSubturnos() {
+		return this.cantidadDeSubturnos;
 	}
-	
-	
-//SETTERS SIMPLES -----------------------------------------------------------------------------------------	
+
+	/**
+	 * pre: --
+	 * post: utiliza un subturno
+	 */
+	public void utilizarSubturno() {
+		this.cantidadDeSubturnos--;
+	}
+
+	//SETTERS SIMPLES -----------------------------------------------------------------------------------------
 }
