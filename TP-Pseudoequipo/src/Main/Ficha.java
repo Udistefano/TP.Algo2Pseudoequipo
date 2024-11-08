7e09919835332550d36d1c743c990e6ecc8a5b76
aclopez <aclopez@fi.uba.ar>
2024-11-08 16:58:20 -0300

    Documentar, y quitar jugador y agregar color a TDA Ficha

--- TP-Pseudoequipo/src/Main/Ficha.java
+++ TP-Pseudoequipo/src/Main/Ficha.java
@@ -3,46 +3,88 @@ package Main;
 public class Ficha {
 //ATRIBUTOS DE CLASE --------------------------------------------------------------------------------------
 //ATRIBUTOS -----------------------------------------------------------------------------------------------
-	 private char simbolo = ' ';
-	 private Jugador jugador = null;
+    private char simbolo = ' ';
+    private String color = null;
 
-	 
-	
 //CONSTRUCTORES -------------------------------------------------------------------------------------------
-	
-	public Ficha(Jugador jugador) {
-		//TODO: validar constructor ficha
-		this.jugador = jugador;
-		this.simbolo = jugador.getSimbolo();
-	}
-	
+
+    /**
+     * pre:
+     * @param simbolo no puede ser vacio
+     * @param color no puede ser vacio ni nulo
+     * @throws Exception si alguno de los parametros es nulo
+     * post: inicializa la ficha con el simbolo y color dados
+     */
+    public Ficha(char simbolo, String color) throws Exception {
+        if (simbolo == ' ') {
+            throw new Exception("El simbolo no puede ser vacio");
+        }
+        if ((color == null) ||
+                (color.isEmpty())) {
+            throw new Exception("El color no puede ser vacio");
+        }
+        this.simbolo = simbolo;
+        this.color = color;
+    }
+
 //METODOS DE CLASE ----------------------------------------------------------------------------------------
 //METODOS GENERALES ---------------------------------------------------------------------------------------
-	
-	@Override
-	public String toString() {
-		return "" + this.simbolo;
-	}
-	
+
+    /**
+     * pre: --
+     * @return una cadena mostrando el simbolo de la ficha
+     */
+    @Override
+    public String toString() {
+        return "" + this.simbolo;
+    }
+
 //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
 //GETTERS SIMPLES -----------------------------------------------------------------------------------------
-	
-	public char getSimbolo() {
-		return this.simbolo;
-	}
-	public Jugador getjugador() {
-		return this.jugador;
-	}
-//SETTERS SIMPLES -----------------------------------------------------------------------------------------	
 
-	public void setSimbolo(char simbolo) {
-		this.simbolo = simbolo;
-	}
+    /**
+     * pre: --
+     * @return el simbolo de la ficha
+     */
+    public char getSimbolo() {
+        return this.simbolo;
+    }
 
-	public void setJugador(Jugador jugador) {
-		this.jugador = jugador;
-	}
-	
-	
-	
+    /**
+     * pre: --
+     * @return el color de la ficha
+     */
+    public String getColor() {
+        return this.color;
+    }
+
+//SETTERS SIMPLES -----------------------------------------------------------------------------------------
+
+    // FIXME: los setters de simbolo y color de Ficha deben ser public o protected?
+    /**
+     * pre:
+     * @param simbolo no puede ser vacio
+     * @throws Exception si simbolo es vacio
+     * post: cambia el simbolo de la ficha al pasado por parametro
+     */
+    public void setSimbolo(char simbolo) throws Exception {
+        if (simbolo == ' ') {
+            throw new Exception("El simbolo no puede ser vacio");
+        }
+        this.simbolo = simbolo;
+    }
+
+    /**
+     * pre:
+     * @param color no puede ser vacio ni nulo
+     * @throws Exception si color es vacio o nulo
+     * post: cambia el color de la ficha al pasado por parametro
+     */
+    public void setColor(String color) throws Exception {
+        if ((color == null) ||
+                (color.isEmpty())) {
+            throw new Exception("El color no puede ser vacio");
+        }
+        this.color = color;
+    }
 }
